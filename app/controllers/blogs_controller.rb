086692ab0a9b491fe0a74443fc207bc73a4eb6b3
松本一輝 <mormorbump@gmail.com>
2017-11-13 00:44:52 +0900

    first

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,40 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs =Blog.all.page(params[:page]).reverse_order
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.save
+    redirect_to blogs_path
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    @blog.update(blog_params)
+    redirect_to blogs_path
+  end
+
+  def destroy
+    @blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+      def blog_params
+        params.require(:blog).permit(:title, :category, :body)
+      end
+end
