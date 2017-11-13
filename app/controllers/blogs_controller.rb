42004a3950dd166ec3e2476870ba861ec6c7861b
松本一輝 <mormorbump@gmail.com>
2017-11-13 14:58:33 +0900

    third

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,7 +28,7 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    @blog.find(params[:id])
+    @blog = Blog.find(params[:id])
     @blog.destroy
     redirect_to blogs_path
   end
